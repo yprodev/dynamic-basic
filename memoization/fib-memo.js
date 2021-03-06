9ca934d19ff6e615f45812d475f28ae332d52728
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-03-06 17:12:47 +0200

    [Memoization] Impelement Fibonacci function with memoization

--- memoization/fib-memo.js
+++ memoization/fib-memo.js
@@ -17,9 +17,33 @@ const fib = (n) => {
 	return fib(n - 1) + fib(n - 2);
 };
 
-console.log(fib(6)); // 8
-console.log(fib(7)); // 13
-console.log(fib(8)); // 21
-console.log(fib(50)); // Takes too long to be completed with a classic implementation
+// console.log(fib(6)); // 8
+// console.log(fib(7)); // 13
+// console.log(fib(8)); // 21
+// console.log(fib(50)); // Takes too long to be completed with a classic implementation
 
+// ==================================================================================
 
+// Memoization
+// 	js object:
+// 		keys will be arg to fn,
+// 		value will be the return value
+
+// Memo object is empty and default value is empty for the top level function call
+const fibMemo = (n, memo = {}) => {
+	// Use already calculated value
+	if (n in memo) return memo[n];
+	// Base case
+	if (n <= 2) return 1;
+	// Store after calculation AND pass the memo object to the next step (very important)
+	memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+
+	return memo[n];
+};
+
+console.log(fibMemo(6)); // 8
+console.log(fibMemo(7)); // 13
+console.log(fibMemo(8)); // 21
+console.log(fibMemo(50)); // 12586269025
+
+// ==================================================================================
